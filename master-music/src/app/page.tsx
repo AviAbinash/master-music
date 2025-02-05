3c017857b9fa3a18daab2b2769bb7fd1c3d27ed8
AviAbinash <abinashtripathy567@gmail.com>
2025-02-05 15:15:56 +0530

    first commit

---
+++ master-music/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+ <main className="main-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.2]">
+    <h1 className="text-2xl text-center">Abinash</h1>
+ </main>
+    
+  );
+}
---
+++ master-music/src/components/Navbar.tsx
@@ -0,0 +1,49 @@
+"use client";
+import React, { useState } from "react";
+import {
+  HoveredLink,
+  Menu,
+  MenuItem,
+  ProductItem,
+} from "@/components/ui/navbar-menu";
+import { cn } from "@/utils/cn";
+import Link from "next/link";
+
+const Navbar = ({ className }: { className?: string }) => {
+  const [active, setActive] = useState<string | null>(null);
+  return (
+    <div
+      className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
+    >
+      <Menu setActive={setActive}>
+        <Link href={"/"}>
+          <MenuItem
+            setActive={setActive}
+            active={active}
+            item="Home"
+          ></MenuItem>
+        </Link>
+        <MenuItem setActive={setActive} active={active} item="courses">
+          <div className="text-sm grid grid-cols-2 gap-10 p-4">
+            <HoveredLink href="/web-dev">All Courses</HoveredLink>
+            <HoveredLink href="/interface-design">
+              Basic Music Theory
+            </HoveredLink>
+            <HoveredLink href="/seo">Advance Composition</HoveredLink>
+            <HoveredLink href="/branding">Song Writing</HoveredLink>
+            <HoveredLink href="/branding">Music Production</HoveredLink>
+          </div>
+        </MenuItem>
+        <Link href={"/contact"}>
+          <MenuItem
+            setActive={setActive}
+            active={active}
+            item="Contact Us"
+          ></MenuItem>
+        </Link>
+      </Menu>
+    </div>
+  );
+};
+
+export default Navbar;
