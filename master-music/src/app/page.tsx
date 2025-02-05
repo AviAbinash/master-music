e9d19cfa295028c1ef5307953af38bf1fe0054cf
AviAbinash <abinashtripathy567@gmail.com>
2025-02-05 16:03:09 +0530

    hero section added

--- master-music/src/app/page.tsx
+++ master-music/src/app/page.tsx
@@ -1,10 +1,10 @@
+import HeroSection from "@/components/HeroSection";
 import Image from "next/image";
 
 export default function Home() {
   return (
- <main className="main-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.2]">
-    <h1 className="text-2xl text-center">Abinash</h1>
- </main>
-    
+    <main className="main-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.2]">
+      <HeroSection />
+    </main>
   );
 }
