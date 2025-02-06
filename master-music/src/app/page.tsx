286d8d191b1cea98624831f8bbcf16e5200f5df0
AviAbinash <abinashtripathy567@gmail.com>
2025-02-06 11:50:40 +0530

    infinite scrollcards added

--- master-music/src/app/page.tsx
+++ master-music/src/app/page.tsx
@@ -1,3 +1,4 @@
+import CarouselCard from "@/components/CarouselCard";
 import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
 import Image from "next/image";
@@ -7,6 +8,7 @@ export default function Home() {
     <main className="main-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.2]">
       <HeroSection />
       <FeaturedCourses/>
+      <CarouselCard/>
     </main>
   );
 }
